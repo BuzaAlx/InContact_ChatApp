254b1a7c539c9a1c2bbb14978426e0d440634113
BuzaAlx <buzakivi@gmail.com>
2021-03-15 16:19:24 +0200

    Init clientSide

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Register from "./pages/Register";
+import { deepPurple } from "@material-ui/core/colors";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ApolloProvider from "./ApolloProvider";
+
+import { MessageProvider } from "./context/message";
+import DynamicRoute from "./helpers/DynamicRoute";
+
+import store from "./redux/createStore";
+import { Provider } from "react-redux";
+
+const darkTheme = createMuiTheme({
+  typography: {
+    fontFamily: "'Space Grotesk', sans-serif;",
+  },
+  palette: {
+    type: "dark",
+    primary: {
+      main: deepPurple[400],
+    },
+  },
+});
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ApolloProvider>
+        <MessageProvider>
+          <BrowserRouter>
+            <ThemeProvider theme={darkTheme}>
+              <div className="App">
+                <Switch>
+                  <DynamicRoute exact path="/" component={Home} authenticated />
+                  <DynamicRoute path="/register" component={Register} guest />
+                  <DynamicRoute path="/login" component={SignIn} guest />
+                </Switch>
+              </div>
+            </ThemeProvider>
+          </BrowserRouter>
+        </MessageProvider>
+      </ApolloProvider>
+    </Provider>
+  );
+}
+
+export default App;
